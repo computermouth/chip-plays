8de7def306dcc944f55bdf4830c53269157e5bf0
computermouth <computermouth@crunchbangplusplus.org>
2016-08-12 18:11:51 -0700

    small identifiers, fcgi daemon

--- fastcgi/main.c
+++ fastcgi/main.c
@@ -1,18 +1,29 @@
-//#include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 #include "fcgi_stdio.h"
-//#include "fcgio.h"
 
 int main(){
 
 	int xpos = 220;
 
-	char qs[10];
+	FCGX_Request request;
+	FCGX_Init();
+	FCGX_InitRequest(&request, 0, 0);
 
-	while(FCGI_Accept() >=0){
+	while(FCGX_Accept_r(&request) >=0){
 		
-		printf("Content-type: text/plain\r\n\r\nStatus: 200 OK\r\n\r\n");
+		printf("Content-type: text/plain\r\n\r\n");
+		
+		const char *qs = FCGX_GetParam("QUERY_STRING", request.envp);
+		
+		if(qs[0] == 'p'){
+			printf("%d", xpos);
+		}else if(qs[0] == 'm'){
+			xpos--;
+		}else if(qs[0] == 'a'){
+			xpos++;
+		}
+		
 	}
 
 	return 0;
