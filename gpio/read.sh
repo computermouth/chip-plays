4fe900289d7134b4fde0d70537de51f6af56c9f0
computermouth <computermouh@crunchbangplusplus.org>
2016-08-13 02:35:49 +0000

    Added gpio scripts

---
+++ gpio/read.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cat /sys/class/gpio/gpio1016/value
+cat /sys/class/gpio/gpio1017/value
---
+++ gpio/set_pins.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo 1016 > /sys/class/gpio/export
+echo 1017 > /sys/class/gpio/export
